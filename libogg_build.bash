95aaa7aa878bd298156b24e5a8d2a3068d5c73f3
idz <idz@iosdeveloperzone.com>
2015-09-09 01:56:48 -0700

    Rollback SDK version to 8.1 for Travis builds.

--- libogg_build.bash
+++ libogg_build.bash
@@ -1,11 +1,12 @@
 IDZ_OGG_VERSION=1.3.2
 
 #Update IDZ_OGG_SDK_VERSION to the version available on your mac
-#Run xcodebuild -showsdks to see the iOS version availanle on your mac
+#Run xcodebuild -showsdks to see the iOS version available on your mac
 echo Available SDKs
 xcodebuild -showsdks
 
-IDZ_OGG_SDK_VERSION=8.3
+#On GitHub this needs to be an SDK version supported by Travis
+IDZ_OGG_SDK_VERSION=8.1
 
 pushd $IDZ_BUILD_ROOT
 mkdir -p libogg/$IDZ_OGG_VERSION
